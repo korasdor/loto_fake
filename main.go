faf3c09ad016933baf6f4f06b978efc3e213d5d0
am_urunov <am_urunov@bingo-boom.ru>
2015-04-24 17:48:22 +0300

    last commit

--- main.go
+++ main.go
@@ -28,13 +28,7 @@ func main() {
 	router.HandleFunc("/game/{game_name}/bets", handlers.PostBetsHandler)
 	router.HandleFunc("/game/{game_name}/bets/{bet_id}/{var_type}", handlers.PostBetChangeHandler)
 	router.HandleFunc("/game/{game_name}/{game_id}/win", handlers.GetWinHandler)
-
 	http.Handle("/", router)
-
-	// http.HandleFunc("/randstream", handlers.RandHandler)
-	// http.HandleFunc("/rules/keno", handlers.RulesKenoHandler)
-	// http.HandleFunc("/game/keno/bets", handlers.GameKenoBetsHandler)
-	// http.HandleFunc("/game/keno/win", handlers.GameKenoWinHandler)
 
 	err := http.ListenAndServe(GetListenAddr(), nil)
 	if err != nil {
